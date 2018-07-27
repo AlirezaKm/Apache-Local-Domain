723d3a7595f7dc1ccdd77ac261f8c950c77b26e8
Alireza Km <alitm28@gmail.com>
2018-07-27 04:47:06 +0430

    [General] - Setup file updated. - add LICENSE file - add README.md file (not ready yet) - .gitignore updated

---
+++ README.md
@@ -0,0 +1,4 @@
+## Apache Local Domain
+
+##### Description
+    Create own Domain on Your Local System
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='Apache-Local-Domain',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(exclude=['.venv']),
     url='',
-    license='MIT',
+    license='GPLv3',
     author='Alireza Km',
     author_email='alitm28@gmail.com',
     description='Create Domain in Localhost on Apache2',

68882e939c5694d509a49097a5297952f354e18c
Alireza Km <alitm28@gmail.com>
2018-07-28 04:09:38 +0430

    [Fix] - setup.py url Fixed

--- setup.py
+++ setup.py
@@ -79,9 +79,7 @@ setup(
     name='Apache-Local-Domain',
     version='1.1.0',
     packages=find_packages(exclude=['.venv', 'build', 'dist', 'Apache_Local_Domain.egg-info']),
-    url={
-        'Source Code': 'https://gitlab.com/toys-projects/Apache-Local-Domain'
-    },
+    url='https://gitlab.com/toys-projects/Apache-Local-Domain',
     license='GPLv3',
     author='Alireza Km',
     author_email='alitm28@gmail.com',
